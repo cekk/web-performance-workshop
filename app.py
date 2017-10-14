c4d849ebcb2de558f93687bae16f1bbaf614834a
Anton <m@e5t.ro>
2017-10-15 00:05:26 +0200

    Add unix socket

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 import tornado.ioloop
 import tornado.web
-import os
+from tornado.options import options, define
+from tornado.netutil import bind_unix_socket
+from tornado.httpserver import HTTPServer
+
+define('unix_socket', default="/tmp/nginx.socket", help='Path to unix socket to bind')
+DEBUG = 'DYNO' not in os.environ
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -9,10 +15,16 @@ class MainHandler(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-    ])
+    ], debug=DEBUG)
 
 if __name__ == "__main__":
     app = make_app()
-    port = os.environ.get("PORT", '5000')
-    app.listen(port)
+    if not DEBUG:
+        server = HTTPServer(app)
+        socket = bind_unix_socket(options.unix_socket)
+        server.add_socket(socket)
+        open('/tmp/app-initialized', 'w').close()
+    else:
+        port = os.environ.get("PORT", '8000')
+        app.listen(port)
     tornado.ioloop.IOLoop.current().start()
