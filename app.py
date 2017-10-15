a0100c4dffc43fa89b1e4fa22b4c3cc81029cf5e
Anton <m@e5t.ro>
2017-10-15 12:50:34 +0200

    Switch to tornado

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import tornado.ioloop
-import tornado.web
+import yaml
+from tornado.web import Application, RequestHandler, StaticFileHandler
 from tornado.options import options, define
 from tornado.netutil import bind_unix_socket
 from tornado.httpserver import HTTPServer
@@ -8,17 +9,21 @@ from tornado.httpserver import HTTPServer
 define('unix_socket', default="/tmp/nginx.socket", help='Path to unix socket to bind')
 DEBUG = 'DYNO' not in os.environ
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(RequestHandler):
     def get(self):
-        self.write("Hola, world")
-
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ], debug=DEBUG)
+        with open('database.yml') as yfile:
+            cats = yaml.load(yfile)
+        self.render("templates/index.html", cats=cats)
 
 if __name__ == "__main__":
-    app = make_app()
+    app = Application(
+        [
+            (r"/", MainHandler),
+            (r"/img/(.*)", StaticFileHandler, {'path': 'img'}),
+            (r"/css/(.*)", StaticFileHandler, {'path': 'css'}),
+            (r"/js/(.*)", StaticFileHandler, {'path': 'js'}),
+            (r"/vendor/(.*)", StaticFileHandler, {'path': 'vendor'}),
+        ], debug=DEBUG)
     if not DEBUG:
         server = HTTPServer(app)
         socket = bind_unix_socket(options.unix_socket)
